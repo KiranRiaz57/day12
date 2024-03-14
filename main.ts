3884dd333c7cc1878f792e79943a043a5760635b
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-14 22:24:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//Q34
+// let pizzas: string[] =["pepperoni","margherita","hawaiian"]
+// pizzas.forEach(pizza =>{
+// console.log(`I like ${pizza} pizza.`)
+// }
+// )
+// console.log("I really love pizza!")
+// Q35
+// let animals: string[] =["dog","cat","rabbit"]
+// animals.forEach(animal=>{
+//     console.log(`A ${animal} would make a great pet.`)
+// })
+// console.log("any of these animals would make a great pet!")
+// Q36
+// function make_shirt(size:string,message: string){
+//     console.log(`making a ${size} t-shirt with the message"${message}"`)
+// }
+// make_shirt("medium","code is life")
